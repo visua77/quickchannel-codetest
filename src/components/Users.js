624808e45731819cba2918b3125311e559dcf1fc
Niclas Binnquist <niclasbinnquist@niclas-binnquists-mac-pro.local>
2021-01-25 13:30:18 +0100

    working on ux

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Users = ({users,user,setUser})=> {
     return(
         <nav>
-        {users.map(usr=>(<span key={usr} className={usr.includes(user) ? "user-nav-active":"user-nav"}
+        <span className="strong">Set User:</span> {users.map(usr=>(<span key={usr} className={usr.includes(user) ? "user-nav-active":"user-nav"}
         onClick={()=> setUser(usr)}>{usr}</span>))}
         </nav>
     )
